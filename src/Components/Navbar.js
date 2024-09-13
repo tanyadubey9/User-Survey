376b308a2f5cfe0e5bace550f9e28388889ef724
tanyadubey9 <tanyadubey5005@gmail.com>
2024-09-13 21:26:35 +0530

    Add application code

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = ({onStart}) => {
+  return (
+    <nav className='bg-[#8b48b8e1] p-3 text-center'>
+      <div><span className='text-green-300 text-2xl'>User</span><span className='text-yellow-300 text-2xl'>Survey!</span></div>
+    </nav>
+  )
+}
+
+export default Navbar
