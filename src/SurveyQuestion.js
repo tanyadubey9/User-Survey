4a55bcc9ce571470223bae5e601e85c5c76c4de6
tanyadubey9 <tanyadubey5005@gmail.com>
2024-09-15 17:49:53 +0530

    changes

--- src/SurveyQuestion.js
+++ src/SurveyQuestion.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './SurveyQuestion.css';
 
-export default function SurveyQuestion({ question, answer, onAnswerChange }) {
+export default function SurveyQuestion({ question, answer, onAnswerChange}) {
     const renderRatingOptions = () => {
         const options = Array.from({ length: question.scale }, (_, i) => i + 1);
         return options.map((option) => (
             <div
                 key={option}
-                className={`rating-box ${answer === option ? 'selected' : ''}`}
+                className={`rating-box ${answer === option ? 'selected' : ''} md:w-10 md:h-10 w-8 h-8 font-semibold border-2 border-[#cccccc] rounded-md flex items-center justify-center cursor-pointer transition-colors duration-300 hover:border-[#333]`}
                 onClick={() => onAnswerChange(question.id, option)}
             >
                 {option}
@@ -17,9 +17,9 @@ export default function SurveyQuestion({ question, answer, onAnswerChange }) {
     return (
         <div className="w-full">
             <div className="mx-auto my-10 bg-[#c88cf0] w-4/5 h-[550px] flex flex-col justify-center items-center gap-8 border-2 border-white rounded-lg">
-                <h2 className="text-2xl">{question.text}</h2>
+                <h2 className="text-2xl font-semibold text-violet-950">{question.text}</h2>
                 {question.type === "rating" ? (
-                    <div className="rating-container flex flex-wrap justify-center mx-6">{renderRatingOptions()}</div>
+                    <div className="rating-container flex flex-wrap justify-center gap-2">{renderRatingOptions()}</div>
                 ) : (
                     <textarea className="rounded-xl border-2 border-violet-600 w-2/5 h-24 p-2"
                         value={answer || ""}
--- src/ThankYouScreen.js
+++ src/ThankYouScreen.js
@@ -1,5 +1,5 @@
 export default function ThankYouScreen() {
-    return <div className="mx-auto my-10 bg-[#c88cf0] w-4/5 h-[650px] flex flex-col justify-center items-center gap-8 border-2 border-white rounded-lg"><h1 className="font-bold text-4xl">Thank you for your time!😊</h1>
+    return <div className="mx-auto my-10 bg-[#c88cf0] w-4/5 h-[650px] flex flex-col justify-center items-center gap-8 border-2 border-white rounded-lg"><h1 className="font-bold text-violet-950 md:text-4xl text-3xl">Thank you for your time!😊</h1>
     </div> ;
   }
   
